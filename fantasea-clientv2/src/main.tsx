c027fda01c003bc09184a7856e1fa526fabe3763
Liri <lirisobol16@gmail.com>
2024-09-02 21:49:04 +0300

    added data fetch effect & loading spinner component

--- fantasea-clientv2/src/main.tsx
+++ fantasea-clientv2/src/main.tsx
@@ -3,7 +3,7 @@ import App from './App.tsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { store } from './store/Store.tsx'
+import { store } from './store/store.ts'
 
 createRoot(document.getElementById('root')!).render(
     <Provider store={store}>
