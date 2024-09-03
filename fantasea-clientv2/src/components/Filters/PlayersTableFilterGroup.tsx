0cacdd17db805c7212b9a5ec9bf894c14fea3049
Liri <lirisobol16@gmail.com>
2024-09-03 17:09:03 +0300

    further styling of filter group

--- fantasea-clientv2/src/components/Filters/PlayersTableFilterGroup.tsx
+++ fantasea-clientv2/src/components/Filters/PlayersTableFilterGroup.tsx
@@ -4,10 +4,10 @@ import { TeamFilter } from "./TeamFilter"
 
 export const PlayersTableFilterGroup = ():JSX.Element => {
     return (
-        <div className="p-1 flex gap-1 justify-center">
+        <div className="fixed top-[64px] z-10 w-full bg-white shadow p-1 flex gap-1 justify-center">
             <SearchFilter />
             <TeamFilter />
             <PositionFilter />
         </div>
     )
-}
+} 
