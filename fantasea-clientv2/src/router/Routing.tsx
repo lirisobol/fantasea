1a4263006e9a31c3b488f81c12be39ca4f0ee6f7
Liri <lirisobol16@gmail.com>
2024-11-03 13:52:39 +0200

    removed all draft previous methods and versions

--- fantasea-clientv2/src/router/Routing.tsx
+++ fantasea-clientv2/src/router/Routing.tsx
@@ -1,13 +1,11 @@
 import { Route, Routes } from "react-router-dom"
 import { Home } from "../pages/Home"
-import { Draft } from "../pages/Draft"
 import { MyFPL } from "../pages/MyFPL"
 
 export const Routing = ():JSX.Element => {
     return (
         <Routes>
             <Route path="/"  element={<Home />} />
-            <Route path="/draft" element={<Draft />} />
             <Route path="/myfpl" element={<MyFPL />} />
         </Routes>
     )
